f679ca5e33ec6c7a6fc0cd13586f98cb99c13f9c
Oliver Leis <oliver.leis@gmail.com>
2024-10-16 19:39:19 +0200

    created custom file where emacs can mess things up

---
+++ custom.el
@@ -0,0 +1,24 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Iosevka" :foundry "outline" :slant normal :weight regular :height 120 :width normal))))
+ '(fixed-pitch ((t nil)))
+ '(markdown-language-keyword-face ((t (:family "Iosevka"))))
+ '(markdown-pre-face ((t (:family "Iosevka")))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
+ '(display-battery-mode t)
+ '(display-line-numbers-type 'relative)
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages
+   '(disk-usage nov org-remark org-modern undo-tree pdf-tools typescript-mode markdown-mode go-mode gruber-darker-theme magit))
+ '(tool-bar-mode nil))
--- init.el
+++ init.el
@@ -1,28 +1,7 @@
-;; Font settings 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka" :foundry "outline" :slant normal :weight regular :height 120 :width normal))))
- '(fixed-pitch ((t nil)))
- '(markdown-language-keyword-face ((t (:family "Iosevka"))))
- '(markdown-pre-face ((t (:family "Iosevka")))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruber-darker))
- '(custom-safe-themes
-   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
- '(display-battery-mode t)
- '(display-line-numbers-type 'relative)
- '(global-display-line-numbers-mode t)
- '(package-selected-packages
-   '(nov org-remark org-modern undo-tree pdf-tools typescript-mode markdown-mode go-mode gruber-darker-theme magit))
- '(tool-bar-mode nil))
+; Load custom file and let emacs spam there
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 ;; Fixed encoding issues on some Windows systems
 (setq-default buffer-file-coding-system 'utf-8)
