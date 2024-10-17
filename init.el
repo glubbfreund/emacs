77ddb6d74af81f6b99e748fa01878d94383495fe
Oliver Leis <oliver.leis@gmail.com>
2024-10-17 15:29:39 +0200

    added eradio package

--- init.el
+++ init.el
@@ -147,3 +147,16 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
 (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+
+;; Install and configure eradio
+(use-package eradio
+  :ensure t)
+(global-set-key (kbd "C-c r p") 'eradio-play)
+(global-set-key (kbd "C-c r s") 'eradio-stop)
+(global-set-key (kbd "C-c r t") 'eradio-toggle)
+(setq eradio-player '("mpv" "--no-video" "--no-terminal"))
+(setq eradio-channels '(("Παλμός Κεφαλλονιάς 98.3" . "http://ice.streamcloud.mediacdn.com/palmos983")        
+                        ("Sfera 102.2"  . "http://sfera.live24.gr/sfera4132")
+                        ("Ράδιο Θεσσαλονίκη 94.5" . "http://eu7.fastcast4u.com:6156/stream?type=http&nocache=136596")
+                        ("Bayern 3" . "http://streams.br.de/bayern3_2.m3u")
+                        ("Bayern 1" . "http://streams.br.de/bayern1_2.m3u"))) 
