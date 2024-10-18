7f7cb404d580d100931be6a67ded9d37eb678128
Oliver Leis <oliver.leis@gmail.com>
2024-10-18 14:24:48 +0200

    even more cleanup

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 ;; Load custom file and let emacs spam there
 (setq custom-file (concat user-emacs-directory "custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file))
+(load custom-file)
 
 ;; Add Melpa repository
 (require 'package)
@@ -29,33 +28,26 @@
 	  auto-save-default nil
       ring-bell-function 'ignore)
 
-;; Set tab-width 
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
-    
-;; Save what is entered into minibuffer prompts
-(setq history-length 25)
+;; activate some wanted features
+(global-auto-revert-mode 1)
+(save-place-mode 1)
+(ido-mode 1)
 (savehist-mode 1)
+(setq gdb-many-windows 1
+      history-length 25
+      ido-enable-flex-matching t
+      ido-use-filename-at-point 'guess
+      ido-everywhere 1)
 
+;; Set tab-width 
+(setq-default tab-width 4)
+    
 ;; Ask for y/n instead of yes/no
-(fset 'yes-or-no-p 'y-or-n-p)
+(setopt use-short-answers t)
 
 ;; clean instruction messages
 (defun display-startup-echo-area-message () (message ""))
 (setq server-client-instructions nil)
-
-;; Save last file position
-(save-place-mode 1)
-
-;; Revert buffers when the underlying file has changed
-(global-auto-revert-mode 1)
-
-;; show the big gdb layout 
-(setq gdb-many-windows 1)
-
-;; set eglot events buffer to 0 to prevent performance issues
-(setq eglot-events-buffer-size 0)
 
 ;; Fixed encoding issues on some Windows systems
 (setq-default buffer-file-coding-system 'utf-8)
@@ -65,11 +57,8 @@
 ;; don't show ANSI escape sequences in compile buffer (Windows issue)
 (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
 
-;; Enable Ido mode
-(setq ido-enable-flex-matching t)
-(setq ido-use-filename-at-point 'guess)
-(setq ido-everywhere 1)
-(ido-mode 1)
+;; set eglot events buffer to 0 to prevent performance issues
+(setq eglot-events-buffer-size 0)
 
 ;; Autorun eglot
 (add-hook 'python-mode-hook 'eglot-ensure)
