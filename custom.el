fa051b84e905981039333a88b65ea49d8cf5ffab
Oliver Leis <oliver.leis@gmail.com>
2025-01-16 10:55:14 +0100

    initial commit for rewrite

--- custom.el
+++ custom.el
@@ -15,11 +15,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
- '(default-input-method "greek")
+   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" default))
  '(display-battery-mode t)
  '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode t)
  '(package-selected-packages
-   '(copilot evil-collection company eglot-java evil org-drill gptel go-translate powershell eradio nov org-remark org-modern undo-tree pdf-tools typescript-mode markdown-mode go-mode gruber-darker-theme magit))
+   '(copilot evil-collection evil markdown-mode undo-tree gruber-darker-theme))
  '(tool-bar-mode nil))
