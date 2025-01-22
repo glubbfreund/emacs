ee594882e464a24588efc85e5873223a02fd4394
Oliver Leis <oliver.leis@gmail.com>
2025-01-22 09:10:53 +0100

    Removed evil; Added devil; Moved gnus; Reworked .gitignore

--- auto.el
+++ auto.el
@@ -19,7 +19,8 @@
  '(display-battery-mode t)
  '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode nil)
+ '(gnus-init-file "~/.emacs.d/gnus.el")
  '(package-selected-packages
-   '(which-key pdf-tools mood-line eradio nov org-modern eglot-java gptel copilot evil-collection evil markdown-mode undo-tree gruber-darker-theme))
+   '(devil which-key pdf-tools mood-line eradio nov org-modern eglot-java gptel copilot markdown-mode undo-tree gruber-darker-theme))
  '(tool-bar-mode nil)
  '(warning-suppress-types '((initialization))))
