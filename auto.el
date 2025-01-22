5d434c657a78c67c52b91ab7a91e73d0d10cbf5d
Oliver Leis <oliver.leis@gmail.com>
2025-01-22 23:26:57 +0100

    Smaller tweaks

--- auto.el
+++ auto.el
@@ -13,9 +13,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(battery-mode-line-format "~%b%p%%")
+ '(battery-mode-line-limit 80)
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("5259cf3d5062ea1b0de4f5a3c550b55e2c4a347b19fa874c8522e6171e6a4840" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
+   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" "5259cf3d5062ea1b0de4f5a3c550b55e2c4a347b19fa874c8522e6171e6a4840" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
  '(display-battery-mode t)
  '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode nil)
