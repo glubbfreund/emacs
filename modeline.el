c8f12f7184b6a35fdc5ebd6271461e39c0c0be50
Oliver Leis <oliver.leis@gmail.com>
2025-01-23 23:05:04 +0100

    small improvements

--- modeline.el
+++ modeline.el
@@ -1,28 +1,24 @@
 ;; My rewritten Vanilla modeline, pretty much default
-;; but with the vc-mode on the right side
+;; but cleaned up a bit
 
 (setq-default mode-line-format
               '("%e"
-                mode-line-front-space
-                mode-line-mule-info
-                mode-line-client
+                mode-line-front-space ;;mode-line-mule-info & mode-line-client removed
                 mode-line-modified
                 mode-line-remote
 		" "
-                mode-line-buffer-identification
-		mode-line-position
-		"   "
+                (:eval (propertize (format-mode-line '("%e" mode-line-buffer-identification)) 'face 'bold))
+		" "
 		mode-line-modes
 		(:eval
                  (let* ((mode (format-mode-line '("%e"
-						  "   "
-						  (vc-mode vc-mode)
-						  " "
-						  )))
+						  "  "
+						  mode-line-position)))
 			(misc (format-mode-line '("%e"
-						   mode-line-misc-info
-						   )))
+						  mode-line-misc-info
+						  (vc-mode vc-mode))))
                         (space `((space :align-to (- (+ right right-fringe right-margin)
                                                      ,(+ (string-width mode) (string-width misc)))))))
-                   (concat (propertize " " 'display space)(propertize misc 'face 'shadow) mode)))
-		))
+                   (concat (propertize " " 'display space)
+			   (propertize misc 'face 'shadow)
+			   mode)))))
