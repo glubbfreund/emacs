f5410f884bbe066211da27c673282f8f19ce328c
Oliver Leis <oliver.leis@gmail.com>
2025-01-27 13:30:41 +0100

    further formatting improvements

--- modeline.el
+++ modeline.el
@@ -1,30 +1,33 @@
 ;; My rewritten Vanilla modeline, pretty much default
 ;; but cleaned up a bit
 
+(defun trim-misc-modeline (str)
+  "Remove all whitespace from STR."
+  (replace-regexp-in-string " +" " " str))
+
 (defun clean-misc-modeline (str)
   "Escape all '%' in STR and remove '[' and ']' to ensure correct formatting."
   (let ((escaped-str (replace-regexp-in-string "%" "%%" str)))
-    (let ((trimmed-escaped-str (replace-regexp-in-string "   +" "  " escaped-str)))
+    (let ((trimmed-escaped-str (trim-misc-modeline escaped-str)))
       (replace-regexp-in-string "\\[\\|\\]" "" trimmed-escaped-str))))
 
 (setq-default mode-line-format
               '("%e"
-                mode-line-front-space ;; mode-line-client removed
+                mode-line-front-space
 		mode-line-mule-info
                 mode-line-modified
                 mode-line-remote
 		" "
-                (:eval (propertize (format-mode-line '("%e" mode-line-buffer-identification)) 'face 'bold))
+                (:eval (string-trim (propertize (format-mode-line '("%e" mode-line-buffer-identification)) 'face 'bold)))
 		" "
 		mode-line-modes
 		(:eval
-                 (let* ((mode (format-mode-line '("%e"
-						  "  "
-						  (vc-mode vc-mode)
-						  mode-line-end-spaces)))
+                 (let* ((mode (clean-misc-modeline (format-mode-line '("%e"
+								       (vc-mode vc-mode)
+								       mode-line-end-spaces))))
 			(misc (clean-misc-modeline (format-mode-line '("%e"
-						  mode-line-misc-info
-						  mode-line-position))))
+								       mode-line-misc-info
+								       mode-line-position))))
                         (space `((space :align-to (- (+ right right-fringe right-margin)
                                                      ,(+ (string-width mode) (string-width misc)))))))
                    (concat (propertize " " 'display space)
