91bf7fc1e557b53a0ad5391ccd0c8a0dc647e3d8
Oliver Leis <oliver.leis@gmail.com>
2025-02-15 12:14:21 +0100

    removed eradio to use emms

--- plugins.el
+++ plugins.el
@@ -4,6 +4,21 @@
   (load-theme 'gruber-darker t)
   :ensure t)
 
+;; EMMS setup
+(use-package emms
+  :config
+  (require 'emms-setup)
+  (require 'emms-player-mplayer)
+  (emms-all)
+  (setq emms-player-list '(emms-player-mplayer))
+  :bind
+  ("C-c e p" . emms-play-dired)
+  ("C-c e s" . emms-stop)
+  ("C-c e l" . emms-play-m3u-playlist)
+  ("C-c e f" . emms-next)
+  ("C-c e b" . emms-previous)
+  :ensure t)
+
 ;; Nov is a package for reading epubs
 (use-package nov
   :init
@@ -18,25 +33,4 @@
   :bind
   ("C-c k o" . gptel)
   ("C-c k a" . gptel-send)
-  :ensure t)
-
-;; Install and configure eradio
-(use-package eradio
-  :init
-  (setq eradio-player '("mpv" "--no-video" "--no-terminal")
-	eradio-channels
-	'(("Palmos"
-	   . "http://ice.streamcloud.mediacdn.com/palmos983")
-	  ("Ellinadiko"
-	   . "https://radio.streamings.gr/proxy/radioellinadiko?mp=/stream")
-	  ("Sfera"
-	   . "http://sfera.live24.gr/sfera4132")
-	  ("Bayern3"
-	   . "http://streams.br.de/bayern3_2.m3u")
-	  ("Bayern1"
-	   . "http://streams.br.de/bayern1_2.m3u")))
-  :bind
-  ("C-c r p" . eradio-play)
-  ("C-c r s" . eradio-stop)
-  ("C-c r t" . eradio-toggle)
   :ensure t)
