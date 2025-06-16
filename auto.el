b3c0c51e64fad954dd517c952ae361a6fab0e39b
Oliver Leis <oliver.leis@gmail.com>
2025-06-16 15:45:18 +0200

    update gitignore added sudo

--- auto.el
+++ auto.el
@@ -3,18 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka" :foundry "outline" :slant normal :weight regular :height 120 :width normal))))
+ '(default ((t (:family "IosevkaTerm Nerd Font" :foundry "UKWN" :slant normal :weight regular :height 151 :width normal))))
  '(fixed-pitch ((t nil)))
- '(markdown-language-keyword-face ((t (:family "Iosevka"))))
- '(markdown-pre-face ((t (:family "Iosevka")))))
+ '(markdown-language-keyword-face ((t (:family "Iosevka Fixed SS09"))))
+ '(markdown-pre-face ((t (:family "Iosevka Fixed SS09")))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(battery-mode-line-format " ~%p%%%b ")
- '(battery-mode-line-limit 79)
+ '(battery-mode-line-limit 78)
  '(custom-safe-themes
    '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
      "5259cf3d5062ea1b0de4f5a3c550b55e2c4a347b19fa874c8522e6171e6a4840"
@@ -25,7 +26,6 @@
  '(display-time-24hr-format t)
  '(display-time-default-load-average nil)
  '(display-time-mode t)
- '(global-display-line-numbers-mode nil)
  '(gnus-init-file "~/.emacs.d/gnus.el")
  '(package-selected-packages
    '(eglot-java emms gptel gruber-darker-theme markdown-mode nov
