ce97adde476bb6711a39d6888bdb2421a21dc3c2
Glubbfreund <oliver.leis@gmail.com>
2025-07-11 16:31:10 +0200

    Changed config according to battery limit

--- auto.el
+++ auto.el
@@ -15,7 +15,7 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(battery-mode-line-format " ~%p%%%b ")
- '(battery-mode-line-limit 78)
+ '(battery-mode-line-limit 58)
  '(custom-safe-themes
    '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
      "5259cf3d5062ea1b0de4f5a3c550b55e2c4a347b19fa874c8522e6171e6a4840"
