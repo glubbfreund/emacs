c46a9594fc2c089083fadaa148ca4632e0553aae
Glubbfreund <oliver.leis@gmail.com>
2025-07-24 18:24:40 +0200

    added auto theme switch for linux/xfce4

--- auto.el
+++ auto.el
@@ -15,7 +15,7 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(battery-mode-line-format " ~%p%%%b ")
- '(battery-mode-line-limit 58)
+ '(battery-mode-line-limit 68)
  '(custom-safe-themes
    '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
      "5259cf3d5062ea1b0de4f5a3c550b55e2c4a347b19fa874c8522e6171e6a4840"
@@ -28,7 +28,7 @@
  '(display-time-mode t)
  '(gnus-init-file (concat user-emacs-directory "gnus.el"))
  '(package-selected-packages
-   '(eglot-java emms gptel gruber-darker-theme markdown-mode nov
-		org-modern sudo-edit))
+   '(auto-dark eglot-java emms gptel gruber-darker-theme markdown-mode
+	       nov org-modern sudo-edit))
  '(tool-bar-mode nil)
  '(warning-suppress-types '((initialization))))
--- plugins.el
+++ plugins.el
@@ -1,8 +1,43 @@
 ;; Use and autoinstall my favourite theme
 (use-package gruber-darker-theme
-  :config
-  (load-theme 'gruber-darker t)
   :ensure t)
+
+;; Auto theme if using linux
+(when (eq system-type 'gnu/linux)
+
+  (defvar my/xfce-theme-check-interval 30
+    "How often we check for theme change.")
+
+  (defvar my/xfce-current-theme ""
+    "Save current theme name.")
+
+  (defun my/xfce-get-current-theme ()
+    "Read current theme."
+    (string-trim
+     (shell-command-to-string
+      "xfconf-query -c xsettings -p /Net/ThemeName 2>/dev/null")))
+
+  (defun my/xfce-theme-sync ()
+    "Check sys theme and set it."
+    (let ((theme (my/xfce-get-current-theme)))
+      (unless (string= theme my/xfce-current-theme)
+        (setq my/xfce-current-theme theme)
+        (message "Theme changed: %s" theme)
+        (cond
+         ((string= theme "Arc-Dark-Custom")
+          (my/load-emacs-theme 'gruber-darker))
+         ((string= theme "Arc-Lighter-Custom")
+          (my/load-emacs-theme 'tsdh-light))
+         (t
+          (message "No theme found for: %s" theme))))))
+
+  (defun my/load-emacs-theme (theme)
+    "Load the current theme and disable others"
+    (mapc #'disable-theme custom-enabled-themes)
+    (load-theme theme t))
+
+  ;; Run every <<intervall>> seconds
+  (run-at-time nil my/xfce-theme-check-interval #'my/xfce-theme-sync))
 
 ;; Allow sudo commands
 (use-package sudo-edit
