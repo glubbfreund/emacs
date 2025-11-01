f420ca1be485cefc8edfb19e3052a92a23292390
Glubbfreund <oliver.leis@gmail.com>
2025-11-01 11:39:15 +0100

    Added minimal git status to modeline

--- modeline.el
+++ modeline.el
@@ -1,6 +1,23 @@
 ;; My rewritten Vanilla modeline, pretty much default
 ;; but cleaned up a bit
 
+(defun vc-statusline-git-summary ()
+  "Return a short, color-coded summary of Git changes for the current repo."
+  (when (and vc-mode (eq (vc-backend buffer-file-name) 'Git))
+    (let* ((status (vc-git--run-command-string nil "status" "--porcelain"))
+           (lines (and status (split-string status "\n" t)))
+           (added 0) (modified 0) (deleted 0))
+      (dolist (l lines)
+        (cond
+         ((string-match-p "^\\?\\?" l) (cl-incf added))
+         ((string-match-p "^ M" l) (cl-incf modified))
+         ((string-match-p "^ D" l) (cl-incf deleted))))
+      (concat
+       (when (> modified 0)
+         (propertize (format " ~%d" modified) 'face 'warning))
+       (when (> deleted 0)
+         (propertize (format " -%d" deleted) 'face 'warning))))))
+
 (defun clean-modeline-str (str)
   "Trim whitespace and remove brackets in STR for modeline."
   (let ((fixed-str (replace-regexp-in-string "%" "%%" str)))
@@ -13,13 +30,14 @@
 		mode-line-mule-info
                 mode-line-modified
                 mode-line-remote
-		" "
+				" "
                 (:eval (propertize (string-trim (format-mode-line mode-line-buffer-identification))
                                    'face 'warning))
                 " "
                 (:eval (clean-modeline-str (format-mode-line mode-line-position)))
-		" "
-                (:eval (string-trim (format-mode-line vc-mode)))
+				" "
+				(:eval (string-trim (format-mode-line vc-mode)))
+				(:eval (vc-statusline-git-summary))
                 ;; Right side: Modes + Misc
                 (:eval
                  (let* ((mode (clean-modeline-str (format-mode-line mode-line-modes)))
