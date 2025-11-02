5ed42586ec65c637cd0d5943439ca6395863657d
Glubbfreund <oliver.leis@gmail.com>
2025-11-02 08:34:56 +0100

    Disabled time and battery for now, dont need it

--- auto.el
+++ auto.el
@@ -21,11 +21,11 @@
 	 "5259cf3d5062ea1b0de4f5a3c550b55e2c4a347b19fa874c8522e6171e6a4840"
 	 "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
 	 default))
- '(display-battery-mode t)
+ '(display-battery-mode nil)
  '(display-line-numbers-type 'relative)
  '(display-time-24hr-format t)
  '(display-time-default-load-average nil)
- '(display-time-mode t)
+ '(display-time-mode nil)
  '(gnus-init-file (concat user-emacs-directory "gnus.el"))
  '(package-selected-packages
    '(eat eglot-java emms gptel gruber-darker-theme markdown-mode nov
